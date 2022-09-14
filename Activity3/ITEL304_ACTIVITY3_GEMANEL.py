a199f1b53da8a0f4d7d79ae478a5dc657bdf0311
raina-gemanel-lspu <101684251+raina-gemanel-lspu@users.noreply.github.com>
2022-09-15 03:07:58 +0800

    Create ITEL304_ACTIVITY3_GEMANEL.py

---
+++ Activity3/ITEL304_ACTIVITY3_GEMANEL.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return """<html><body>
+                    <h1>MAY I ASK YOU A QUESTION?</h1>
+                    <form action='/greet'>
+                        What is your Name? <input type='text' name='name'><br>
+                        May I know one of your Characteristics? <input type='text' name='character'><br>
+                        <input type='submit' value='Submit'>
+              </body></html>
+           """
+
+@app.route('/greet')
+def greet():
+    name = request.args.get('name')
+    character = request.args['character']
+    if character == '':
+        msg = 'You do not want to tell me? Okay Fine'
+    else:
+        msg = character
+
+    return """
+        <html><body>
+                    <h2>{0} is {1}<h2>
+              </body></html>
+           """.format(name, msg)
+
+
+# Launch the Flask dev server
+app.run(host="localhost", debug=True)
